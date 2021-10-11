e3c97264fc2ff431056e6ecf75da8c4975c62fa1
Alisson Magalhaes <alisson08silva@gmail.com>
2021-10-11 18:02:12 -0300

    Query methods

--- src/main/java/com/devsuperior/jparepository/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/jparepository/repositories/UserRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.jparepository.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.jparepository.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	//repositorio onde acessamos o banco junto com jpa para levar os valores para os controllers
+
+	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+	Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	@Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<User> searchName(String name, Pageable pageable);
+	
+	Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
+	
 
 }
